cdae2d77fa69fbd67dd283a3ea142f180c78180e
Samuel Kunst <samuel@kunst.me>
2025-10-25 09:56:53 +0200

    remove temporary testing code

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
+	"path/filepath"
 	"strconv"
 
+	"github.com/fatih/color"
 	"github.com/toalaah/hn/internal/app"
 	"github.com/toalaah/hn/pkg/hn"
 )
@@ -16,38 +17,23 @@ func main() {
 		err error
 		t   *hn.Story
 	)
-	if len(os.Args) > 1 {
-		id, err = strconv.Atoi(os.Args[1])
-		if err != nil {
-			fmt.Printf("Error opening file: %s\n", err)
-			os.Exit(1)
-		}
+
+	color.Unset()
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s [id]\n\nUnexpected number of args.\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
 	}
 
-	switch id {
-	case 0:
-		f, err := os.Open("./hn.json")
-		if err != nil {
-			fmt.Printf("Error opening file: %s\n", err)
-			os.Exit(1)
-		}
-		defer f.Close()
-		body, err := io.ReadAll(f)
-		if err != nil {
-			fmt.Printf("Error reading file: %s\n", err)
-			os.Exit(1)
-		}
-		t, err = hn.NewThreadFromData(body)
-		if err != nil {
-			fmt.Printf("Error fetching thread: %s\n", err)
-			os.Exit(1)
-		}
-	default:
-		t, err = hn.NewThread(id)
-		if err != nil {
-			fmt.Printf("Error fetching thread: %s\n", err)
-			os.Exit(1)
-		}
+	id, err = strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("Could not parse id: %s\n", err)
+		os.Exit(1)
+	}
+
+	t, err = hn.NewThread(id)
+	if err != nil {
+		fmt.Printf("Error fetching thread: %s\n", err)
+		os.Exit(1)
 	}
 
 	if err := app.Run(t); err != nil {
